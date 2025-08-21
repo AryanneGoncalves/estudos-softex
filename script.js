b2dacfac9388d72d39c4fec65f381282ab2c1af9
Aryanne Gonçalves <aryannegoncalves529@gmail.com>
2025-08-21 15:09:20 -0300

    processo e cração do repositorio

---
+++ script.js
@@ -0,0 +1,5 @@
+function mostrarMensagem() {
+    const paragrafo = document.getElementById("mensagem");
+    paragrafo.textContent = "me nome é Aryanne e meus hobbies são ler livros, escutar música e fazer crochêe minha cor favorita é lilás"
+}
+
